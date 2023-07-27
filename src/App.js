7c3d3bc3b591b515e86d59f1d2d6a0c13b034b57
GabrielAmade <104569217+GabrielAmade@users.noreply.github.com>
2023-07-27 22:16:27 +0200

    traduction anglais

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Profile from "./PortfolioContainer/Home/Profile"
 import "./App.css"
+import Profile from "./PortfolioContainer/Home/Profile"
+import Heading from "./PortfolioContainer/Heading/Heading"
 
 
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <Heading/>
     </div>
   );
 }
--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-// import Typical from "react-typical";
 import "./Profile.css";
-
 import profilephoto from "../../assets/Home/profilephoto.jpg";
-import data from "../../datas.json"; 
+import data_fr from "../../datas_fr.json";
+import data_en from "../../datas_en.json";
 
 const Profile = () => {
+  const [language, setLanguage] = useState("fr");
+  const data = language === "en" ? data_en : data_fr;
+
+  useEffect(() => {
+    const detectedLanguage = navigator.language.split("-")[0];
+    setLanguage(detectedLanguage);
+  }, []);
+
   return (
     <div className="profile-container">
       <div className="profile-columns">
@@ -15,7 +22,7 @@ const Profile = () => {
           <div className="profile-role">
             <h1>{data.greeting}</h1>
             <span>
-              <p className="profile-role-tagline">{data.name}</p>
+              <p className="profile-role-tagline">{data.tagline}</p>
             </span>
           </div>
 
