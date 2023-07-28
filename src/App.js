caf011832e2e6326e8f4fa549040d5755a4b30a9
GabrielAmade <104569217+GabrielAmade@users.noreply.github.com>
2023-07-28 08:48:14 +0200

    navbar responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Profile from "./PortfolioContainer/Home/Profile"
 import Heading from "./PortfolioContainer/Heading/Heading"
+import Navbar from "./PortfolioContainer/Navbar/Navbar"
 
 
 function App() {
   return (
     <div className="App">
+  
+      <Navbar/>
       <Profile/>
       <Heading/>
     </div>
---
+++ src/PortfolioContainer/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <span>Gabriel Amadé</span>
+      </div>
+      <div className="navbar-right">
+        <a href="#apropos">À Propos</a>
+        <a href="#experiences">Expériences</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+      <div className="navbar-underline"></div>
+      </>
+  );
+};
+
+export default Navbar;
