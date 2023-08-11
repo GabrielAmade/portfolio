f46ed511f1138d31cdf13eec22b488e31ebf8c37
GabrielAmade <104569217+GabrielAmade@users.noreply.github.com>
2023-08-11 10:28:42 +0200

    nouvelles corrections resume

--- src/PortfolioContainer/Resume/Resume.jsx
+++ src/PortfolioContainer/Resume/Resume.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Heading from "../Heading/Heading";
-import "./Resume.css"
-
+import "./Resume.css";
 
 function Resume() {
   const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
@@ -53,43 +52,58 @@ function Resume() {
   const projectsDetails = [
     {
       title: (
-        <a href="https://github.com/GabrielAmade/bookie" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/GabrielAmade/bookie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Créez la page d'accueil d'une agence de voyage
         </a>
       ),
       duration: { fromDate: "2020", toDate: "2021" },
       description: (
         <div className="project-container">
-      <div className="project-description project-description-1">
-        Intégrer la maquette du site en HTML et CSS // Respecter le visuel fourni // Rendre l’interface parfaitement responsive pour smartphone et tablette
-      </div>
-      <div className="project-image project-image-1"></div> 
-    </div>
+          <div className="project-description project-description-1">
+            Intégrer la maquette du site en HTML et CSS // Respecter le visuel
+            fourni // Rendre l’interface parfaitement responsive pour smartphone
+            et tablette
+          </div>
+          <div className="project-image project-image-1"></div>
+        </div>
       ),
     },
     {
       title: (
-        <a href="https://github.com/GabrielAmade/Portfolio-architecte-sophie-bluel-master" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/GabrielAmade/Portfolio-architecte-sophie-bluel-master"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Créez une page web dynamique
         </a>
       ),
       duration: { fromDate: "2020", toDate: "2021" },
-      
 
-        description: (
-          <div className="project-container">
-      <div className="project-description project-description-2">
-        Créer la page de présentation des travaux d'une architecte // Ajouter une page de connexion de l'administrateur du site // Afficher une fenêtre modale permettant d’ajouter et de supprimer de nouveaux projets
-      </div>
-      <div className="project-image project-image-2"></div> 
-    </div>
-        ),
+      description: (
+        <div className="project-container">
+          <div className="project-description project-description-2">
+            Créer la page de présentation des travaux d'une architecte //
+            Ajouter une page de connexion de l'administrateur du site //
+            Afficher une fenêtre modale permettant d’ajouter et de supprimer de
+            nouveaux projets
+          </div>
+          <div className="project-image project-image-2"></div>
+        </div>
+      ),
     },
-
 
     {
       title: (
-        <a href="https://github.com/GabrielAmade/kasa" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/GabrielAmade/kasa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Créez une application web de location immobilière
         </a>
       ),
@@ -97,27 +111,32 @@ function Resume() {
       description: (
         <div className="project-container">
           <div className="project-description project-description-3">
-            Créer les pages et les composants du site // Mettre en place les routes pour connecter ces pages // Ajouter des animations de carrousel et de collapse
+            Créer les pages et les composants du site // Mettre en place les
+            routes pour connecter ces pages // Ajouter des animations de
+            carrousel et de collapse
           </div>
-          <div className="project-image project-image-3"></div> 
+          <div className="project-image project-image-3"></div>
         </div>
       ),
     },
-];
+  ];
 
   const resumeDetails = [
     <div className="resume-screen-container" key="education">
       <ResumeHeading
         heading={"Formation “Développeur Web” avec Open Classroom - Alternance"}
-        subHeading={"400 heures de formation sur 12 mois // Formation en distancielle // Principales notions abordées : HTML-CSS, Javascript, React, Node JS, Méthode Agile"}
+        subHeading={
+          "400 heures de formation sur 12 mois // Formation en distancielle // Principales notions abordées : HTML-CSS, Javascript, React, Node JS, Méthode Agile"
+        }
         fromDate={"2022"}
         toDate={"2023"}
       />
 
-
       <ResumeHeading
         heading={"Formation Développeur Web Full Stack"}
-        subHeading={"Formation en présentiel // Apprentissage des bases du HTML/CSS, JS, PHP et Laravel //  Programmation Orientée Objet // Utilisation d'API"}
+        subHeading={
+          "Formation en présentiel // Apprentissage des bases du HTML/CSS, JS, PHP et Laravel //  Programmation Orientée Objet // Utilisation d'API"
+        }
         fromDate={"04-"}
         toDate={"07/22"}
       />
@@ -151,9 +170,7 @@ function Resume() {
             - Gestion et création de sites Wordpress.{" "}
           </span>
           <br />
-          <span className="resume-description-text">
-            - Audit SEO complets.
-          </span>
+          <span className="resume-description-text">- Audit SEO complets.</span>
           <br />
         </div>
       </div>
@@ -233,7 +250,7 @@ function Resume() {
         />
         <span className="bullet-label">{bullet.label}</span>
       </div>
-    ))
+    ));
   };
 
   const getResumeScreen = () => {
@@ -249,14 +266,13 @@ function Resume() {
 
   return (
     <>
-      <div id='experiences'></div>
+      <div id="experiences"></div>
       <Heading title={"Expériences"} />
       <div className="resume-container screen-container">
         <div className="resume-content">
           <div className="resume-card">
             <div className="resume-bullets">
               <div className="bullet-container">
-
                 <div className="bullet-icons"></div>
                 <div className="bullets">{getBullets()}</div>
               </div>
