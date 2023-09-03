fa2b8397b71c37ff122fca698d7ec4d69e06f3da
GabrielAmade <104569217+GabrielAmade@users.noreply.github.com>
2023-09-03 16:24:23 +0200

    responsive carousel

--- src/PortfolioContainer/TestiCarousel/TestiCarousel.jsx
+++ src/PortfolioContainer/TestiCarousel/TestiCarousel.jsx
@@ -22,18 +22,18 @@ const TestiCarousel = () => {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5,
+      items: 1,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      breakpoint: { max: 3000, min: 1400 },
+      items: 1,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      breakpoint: { max: 1400, min: 700 },
+      items: 1,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 700, min: 0 },
       items: 1,
     },
   };
